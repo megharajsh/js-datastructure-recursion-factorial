962e3b1501be6add8d7ae409e3d3b42b5148fd93
megharajsh <megharaj.sh@gmail.com>
2020-05-23 02:18:42 +0530

    1 file modified

--- index.js
+++ index.js
@@ -3,24 +3,31 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1>JS Starter</h1><hr>`;
+
+
+appDiv.innerHTML += `<strong>Compute Factorial: </strong> <input type="text" value="10" id="factNum" onchange="computeFactorial()"/>`;
 
 function computeThruLoop(num) {
   var result = 1;
   for (let i=2; i<=num; i++) {
     result *= i;
   }
-
   return result;
 }
 
-function computeFactorial(num) {
+function computeFactorial() {
+  var factNum = document.getElementById('factNum').value;
+  console.log('Factorial Result: '+ computeThruLoop(factNum));
+  console.log('Factorial Result: '+ computeFactorialUsingRecursion(factNum));
+}
+
+function computeFactorialUsingRecursion(num) {
   if (num === 1) {
     return 1;
   } else {
-    return num * computeFactorial(num-1)
+    return num * computeFactorialUsingRecursion(num-1);
   }
 }
 
-console.log('Factorial Result: '+ computeThruLoop(6));
-console.log('Factorial Result: '+ computeFactorial(6));
+computeFactorial();
